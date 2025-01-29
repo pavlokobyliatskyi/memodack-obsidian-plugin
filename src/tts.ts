7fb5506c91dae826f5bd0c9c15109a01dd4e6980
Pavlo Kobyliatskyi <pavlokobyliatskyi@gmail.com>
2025-01-29 10:42:28 +0200

    feat: add tts

---
+++ src/tts.ts
@@ -0,0 +1,40 @@
+import { requestUrl } from "obsidian";
+
+export class TTS {
+  private apiKey: string;
+
+  constructor(apiKey: string) {
+    this.apiKey = apiKey;
+  }
+
+  async tts(source: string, text: string): Promise<string | null> {
+    try {
+      const url = `https://texttospeech.googleapis.com/v1/text:synthesize?key=${this.apiKey}`;
+
+      const requestBody = {
+        input: { text: text },
+        voice: {
+          languageCode: source,
+        },
+        audioConfig: { audioEncoding: "MP3" },
+      };
+
+      const response = await requestUrl({
+        method: "POST",
+        url,
+        contentType: "application/json",
+        body: JSON.stringify(requestBody),
+      });
+
+      const json = await response.json;
+
+      if (!json?.audioContent) {
+        return null;
+      }
+
+      return json.audioContent || null;
+    } catch (e) {
+      return null;
+    }
+  }
+}
